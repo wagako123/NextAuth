ca6cec5f0516a3ae32d9b396128cf15e57864aa3
unknown <brianwagako@gmail.com>
2023-10-02 09:57:50 +0300

    weird page glitch

---
+++ src/app/api/auth/[...nextauth]/routes.js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import {PrismaAdaper} from "@auth/prisma-adapter"
+import {PrismaClient} from "@prisma/client"
+
+const prisma = new PrismaClient();
+
+export const authOptions ={
+    adapter: PrismaAdaper(prisma)
+    providers: [
+        CredentialsProvider({
+            name:"credentials",
+            credentials:{
+                username:{label:"Username", type:"text", placeholder:"wageks"},
+                password:{label:"Password", type:"password"},
+            },
+            async authorize (credentials){
+                
+            }
+        })
+    ],
+    session:{
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === "development",
+};
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST}
---
+++ src/app/api/register/route.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma =  new PrismaClient();
+
+export async function POST(request){
+    const body= await request.json;
+    const {name, email, password} = body;
+    console.log (body);
+    
+}
